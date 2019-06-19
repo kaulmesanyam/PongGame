d3aebc57641c997c189b3ad9d1087ffbbeee74b3
sanyam-kaul <kaulsanyam@gmail.com>
2019-06-19 19:04:20 +0530

    Updated the bounce effect if statements in update function by using the getters

--- pong.js
+++ pong.js
@@ -65,10 +65,10 @@ function update(dt) {
 	ball.pos.x += ball.vel.x * dt;
 	ball.pos.y += ball.vel.y * dt;
 
-	if(ball.pos.x < 0 || ball.pos.x > canvas.width) {
+	if(ball.left < 0 || ball.right > canvas.width) {
 		ball.vel.x = -ball.vel.x;
 	}
-	if(ball.pos.y < 0 || ball.pos.y > canvas.height) {
+	if(ball.top < 0 || ball.bottom > canvas.height) {
 		ball.vel.y = -ball.vel.y;
 	}
 
