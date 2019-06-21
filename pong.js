2ffc26c04e2bf37d8687cc4578ff89c0df99f110
sanyam-kaul <kaulsanyam@gmail.com>
2019-06-21 16:48:30 +0530

    Coded random ness in the ball bounce back trajectory

--- pong.js
+++ pong.js
@@ -99,7 +99,10 @@ class Pong
 	{
 		if (player.left < ball.right && player.right > ball.left && 
 			player.top < ball.bottom && player.bottom > ball.top) {
+			const len = ball.vel.len;
 			ball.vel.x = -ball.vel.x;
+			ball.vel.y += 300 * (Math.random() - .5);
+			ball.vel.len = len * 1.05;
 		}
 	}
 
