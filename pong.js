5a392d8e4b3fef15ee1c1d330140ea7bfaacf6fe
sanyam-kaul <kaulsanyam@gmail.com>
2019-06-21 16:40:42 +0530

    Coded consistent speed to the ball (200)

--- pong.js
+++ pong.js
@@ -5,6 +5,16 @@ class Vec
 		this.x = x;
 		this.y = y;
 	}
+	get len()
+	{
+		return Math.sqrt(this.x * this.x + this.y * this.y);
+	}
+	set len(value)
+	{
+		const fact = value / this.len;
+		this.x *= fact;
+		this.y *= fact;
+	}
 }
 
 
@@ -119,8 +129,9 @@ class Pong
 	start()
 	{
 		if (this.ball.vel.x === 0 && this.ball.vel.y === 0) {
-		this.ball.vel.x = 300 * (Math.random() > .5 ? 1 : -1);
-		this.ball.vel.y = 300 * (Math.random() * 2 - 1);
+			this.ball.vel.x = 300 * (Math.random() > .5 ? 1 : -1);
+			this.ball.vel.y = 300 * (Math.random() * 2 - 1);
+			this.ball.vel.len = 200;
 		} 
 	}
 	update(dt) {
